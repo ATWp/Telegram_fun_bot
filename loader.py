e8c8558e49c32165955dcc3e59b1616d5d1e3aac
ATWp <69421094+ATWp@users.noreply.github.com>
2020-09-10 12:45:13 +0300

    delete_bad_parametrs

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 
-from config import TOKEN, FOLDER_PICTURES, DATA_FOLDER
+from config import TOKEN, FOLDER_PICTURES
 
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
